5695367c0bb89a80ed5e3a7fa074825a99c8814e
ps173 <prathamsharma173@gmail.com>
2021-06-12 18:24:12 +0530

    Commited Changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,78 +10,15 @@ function App() {
       <Header>
         <Header.Container>
           <Header.Logo> Vim Gore</Header.Logo>
+          <Header.LinksContainer>
+            <Header.Links> Learn </Header.Links>
+            <Header.Links> Faqs </Header.Links>
+          </Header.LinksContainer>
         </Header.Container>
       </Header>
       <FaqContainer />
     </>
   );
-  // const [snippets, setSnippets] = useState([]);
-  // const [globalScore, setGlobalScore] = useState(0);
-  // // const handleRefresh = (e) => {
-  // //   fetch(api_url)
-  // //     .then((res) => res.json())
-  // //     .then((json) => setSnippets(json));
-  // // };
-  // // useEffect(() => {
-  // //   fetch(api_url)
-  // //     .then((res) => res.json())
-  // //     .then((json) => setSnippets(json));
-  // // }, []);
-  // if (snippets.length === 0) {
-  //   console.log(snippets);
-  //   return (
-  //     <div className="container">
-  //       <div className="lds-dual-ring"></div>
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <Router>
-  //       <div className="navbar">
-  //         <Link className="links" to="/">
-  //           {" "}
-  //           <h1> VimGore </h1>{" "}
-  //         </Link>
-  //         <nav className="navbar-links">
-  //           <li>
-  //             <Link className="links" to="/faqs">
-  //               {" "}
-  //               Faqs{" "}
-  //             </Link>
-  //           </li>
-  //           <li>
-  //             <Link className="links" to="/learn-keys">
-  //               {" "}
-  //               Learn{" "}
-  //             </Link>
-  //           </li>
-  //         </nav>
-  //       </div>
-  //       <Switch>
-  //         <Route exact path="/">
-  //           {/* <button className="refresh-cta" onClick={handleRefresh}>
-  //             {" "}
-  //             refresh for new question{" "}
-  //           </button> */}
-  //           <div className="game-area">
-  //             <ApiContext.Provider
-  //               value={{ snippets, globalScore, setGlobalScore }}
-  //             >
-  //               <Editor />
-  //               <TaskInfo />
-  //             </ApiContext.Provider>
-  //           </div>
-  //         </Route>
-  //         <Route path="/faqs">
-  //           <Faqs />
-  //         </Route>
-  //         <Route path="/learn-keys">
-  //           <KeyBinds />
-  //         </Route>
-  //       </Switch>
-  //     </Router>
-  //   );
-  // }
 }
 
 export default App;
--- client/src/pages/GameArea.js
+++ client/src/pages/GameArea.js
@@ -1,6 +1,6 @@
 import React from "react";
 // eslint-disable-next-line
-import { Editor } from "../components";
+// import { Editor } from "../components";
 
 export default function GameArea() {
   return <div></div>;
