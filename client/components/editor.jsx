15aa81ddc1d3fa5936f017e8c0d32f3bd67437b7
ps173 <prathamsharma173@gmail.com>
2021-09-30 11:48:46 +0530

    fixing the auto change

--- client/components/editor.jsx
+++ client/components/editor.jsx
@@ -22,71 +22,45 @@ import "codemirror/mode/clojure/clojure";
 import "codemirror/mode/markdown/markdown";
 
 const VimEditor = (
-  { setKeystrokes, keystrokes, language, ...restProps },
+ { vimode, keystrokes, language, ...restProps },
 ) => {
-  const [vimode, setVimode] = useState("Normal");
+ const localtheme = localStorage.getItem("default-theme")
+  ? localStorage.getItem("default-theme")
+  : "material-palenight";
 
-  const localtheme = localStorage.getItem("default-theme")
-    ? localStorage.getItem("default-theme")
-    : "material-palenight";
+ // Note: Parse is basically to convert the stupid string to bool
+ const localLineNumber = localStorage.getItem("linenumber")
+  ? JSON.parse(localStorage.getItem("linenumber"))
+  : false;
 
-  // Note: Parse is basically to convert the stupid string to bool
-  const localLineNumber = localStorage.getItem("linenumber")
-    ? JSON.parse(localStorage.getItem("linenumber"))
-    : false;
+ const option = {
+  theme: localtheme, // retrieve from localStorage if the theme exists
+  mode: language,
+  lineNumbers: localLineNumber,
+  lineWrapping: false,
+  keyMap: "vim",
+ };
 
-  const handleVimode = (editor) => {
-    if (
-      editor.state.vim.insertMode === false &&
-      editor.state.vim.visualMode === false
-    ) {
-      setVimode("Normal");
-    } else if (
-      editor.state.vim.visualMode === true &&
-      editor.state.vim.insertMode === false
-    ) {
-      setVimode("Visual");
-    } else {
-      setVimode("Insert");
-    }
-  };
-
-  const handleKeyStrokes = () => {
-    setKeystrokes((prev) => prev + 1);
-  };
-
-  const option = {
-    theme: localtheme, // retrieve from localStorage if the theme exists
-    mode: language,
-    lineNumbers: localLineNumber,
-    lineWrapping: false,
-    keyMap: "vim",
-  };
-
-  return (
-    <div className="col-span-2">
-      <CodeMirror
-        {...restProps}
-        className="m-4 rounded-xl"
-        options={option}
-        onKeyHandled={(editor, _data, _value) => {
-          handleVimode(editor);
-          handleKeyStrokes();
-        }}
-      />
-      <div
-        className="m-4 p-2 flex flex-row justify-around align-middle  bg-gradient-to-r from-gray-800 via-indigo-800 to-indigo-900  rounded-md"
-      >
-        <div className="text-xl px-5 font-bold text-red-500">{vimode}</div>
-        {keystrokes
-          ? <div className="text-xl px-5 font-bold text-green-500">
-            Keystrokes : {keystrokes}
-          </div>
-          : " "}
-        <div className="text-xl px-5 font-bold text-red-500">{language}</div>
-      </div>
-    </div>
-  );
+ return (
+  <div className="col-span-2">
+   <CodeMirror
+    {...restProps}
+    className="m-4 rounded-xl"
+    options={option}
+   />
+   <div
+    className="m-4 p-2 flex flex-row justify-around align-middle  bg-gradient-to-r from-gray-800 via-indigo-800 to-indigo-900  rounded-md"
+   >
+    <div className="text-xl px-5 font-bold text-red-500">{vimode}</div>
+    {keystrokes
+     ? <div className="text-xl px-5 font-bold text-green-500">
+      Keystrokes : {keystrokes}
+     </div>
+     : " "}
+    <div className="text-xl px-5 font-bold text-red-500">{language}</div>
+   </div>
+  </div>
+ );
 };
 
 export default VimEditor;
--- client/containers/GameContainer.jsx
+++ client/containers/GameContainer.jsx
@@ -3,116 +3,141 @@ import { GameAreaInfo } from "../components";
 import dynamic from "next/dynamic";
 const VimEditor = dynamic(import("../components/editor"), { ssr: false });
 const DisplayEditor = dynamic(import("../components/display-editor"), {
-  ssr: false,
+ ssr: false,
 });
 import {
-  RedditIcon,
-  RedditShareButton,
-  TelegramIcon,
-  TelegramShareButton,
-  TwitterIcon,
-  TwitterShareButton,
+ RedditIcon,
+ RedditShareButton,
+ TelegramIcon,
+ TelegramShareButton,
+ TwitterIcon,
+ TwitterShareButton,
 } from "react-share";
 import _config_ from "../_config_";
 
 const GameArea = () => {
-  const [code, setCode] = useState(`console.log("somethinghere")`);
-  const [answer, setAnswer] = useState(`console.log("something here")`);
-  const [keystrokes, setKeystrokes] = useState(0);
-  const [language, setLanguage] = useState("javascript");
-  const [globalScore, setGlobalScore] = useState(0);
-  const [reqkeystrokes, setReqKeystrokes] = useState(0);
-  const APIPATH = _config_.API_PATH;
-  const ScoreShareString =
-    `Here Checkout Vimgore and score your vim efficency. My score was ${globalScore}`;
+ const [vimode, setVimode] = useState("Normal");
+ const [code, setCode] = useState(`console.log("somethinghere")`);
+ const [answer, setAnswer] = useState(`console.log("something here")`);
+ const [keystrokes, setKeystrokes] = useState(0);
+ const [language, setLanguage] = useState("javascript");
+ const [globalScore, setGlobalScore] = useState(0);
+ const [reqkeystrokes, setReqKeystrokes] = useState(0);
+ const APIPATH = _config_.API_PATH;
+ const ScoreShareString =
+  `Here Checkout Vimgore and score your vim efficency. My score was ${globalScore}`;
 
-  const fetchData = () => {
-    fetch(APIPATH)
-      .then((response) => response.json())
-      .then((data) => {
-        setCode(data.code);
-        setAnswer(data.answer);
-        setReqKeystrokes(data.minKeyStrokes);
-        setLanguage(data.language);
-      });
-  };
+ const fetchData = () => {
+  fetch(APIPATH)
+   .then((response) => response.json())
+   .then((data) => {
+    setCode(data.code);
+    setAnswer(data.answer);
+    setReqKeystrokes(data.minKeyStrokes);
+    setLanguage(data.language);
+   });
+ };
 
-  // NOTE: Somehow this fixes everything
-  const handleChange = (_e, _d, val) => {
-    if (val.trim() == answer.trim()) {
-      if (keystrokes <= reqkeystrokes) {
-        setGlobalScore((prev) => prev + 5);
-      } else {
-        setGlobalScore((prev) => prev + 1);
-      }
-      setKeystrokes(0);
-    }
-  };
+ // NOTE: Current ViMode
+ const handleVimode = (editor) => {
+  if (
+   editor.state.vim.insertMode === false &&
+   editor.state.vim.visualMode === false
+  ) {
+   setVimode("Normal");
+  } else if (
+   editor.state.vim.visualMode === true &&
+   editor.state.vim.insertMode === false
+  ) {
+   setVimode("Visual");
+  } else {
+   setVimode("Insert");
+  }
+ };
 
-  // Change Question Everytime You have a correct answer
-  useEffect(() => {
-    fetchData();
-  }, [globalScore]);
+ const handleKeyStrokes = () => {
+  setKeystrokes((prev) => prev + 1);
+ };
 
-  // FetchData for first time
-  useEffect(() => {
-    fetchData();
-  }, []);
+ // NOTE: Incrementing the score triggers the auto-changing
+ const handleChange = (_e, _d, val) => {
+  if (val == answer) {
+   if (keystrokes <= reqkeystrokes) {
+    setGlobalScore((prev) => prev + 5);
+   }
+   setGlobalScore((prev) => prev + 1);
+   setKeystrokes(0);
+  }
+ };
 
-  return (
-    <>
-      <GameAreaInfo.Grid>
-        <VimEditor
-          value={code}
-          onChange={handleChange}
-          setKeystrokes={setKeystrokes}
-          keystrokes={keystrokes}
-          language={language}
-        />
-        <GameAreaInfo.Container>
-          <GameAreaInfo.Box>
-            <GameAreaInfo.Text>
-              change the above to text to snippet given below
-            </GameAreaInfo.Text>
-            <DisplayEditor
-              className="code-display"
-              value={answer}
-              language={language}
-            />
-          </GameAreaInfo.Box>
-          <GameAreaInfo.ScoreContainer>
-            <GameAreaInfo.HugeScoreText>
-              Keystrokes required to change : {reqkeystrokes}
-            </GameAreaInfo.HugeScoreText>
-            <GameAreaInfo.HugeScoreText>
-              Global Score : {globalScore}
-            </GameAreaInfo.HugeScoreText>
-          </GameAreaInfo.ScoreContainer>
-          <div className="flex flex-row justify-around">
-            <div className="text-lg px-3 text-purple-100">Share Score :</div>
-            <RedditShareButton
-              url="https://vimgore.netlify.app"
-              title={ScoreShareString}
-            >
-              <RedditIcon className="rounded-full mx-1 w-10 h-10" />
-            </RedditShareButton>
-            <TwitterShareButton
-              url="https://vimgore.netlify.app"
-              title={ScoreShareString}
-            >
-              <TwitterIcon className="rounded-full mx-1 w-10 h-10" />
-            </TwitterShareButton>
-            <TelegramShareButton
-              url="https://vimgore.netlify.app"
-              title={ScoreShareString}
-            >
-              <TelegramIcon className="rounded-full mx-1 w-10 h-10" />
-            </TelegramShareButton>
-          </div>
-        </GameAreaInfo.Container>
-      </GameAreaInfo.Grid>
-    </>
-  );
+ // NOTE: Change Question Everytime You have a correct answer
+ useEffect(() => {
+  fetchData();
+ }, [globalScore]);
+
+ // NOTE: FetchData for first time
+ useEffect(() => {
+  fetchData();
+ }, []);
+
+ return (
+  <>
+   <GameAreaInfo.Grid>
+    <VimEditor
+     value={code}
+     onChange={handleChange}
+     keystrokes={keystrokes}
+     onKeyHandled={(editor, _data, _value) => {
+      handleVimode(editor);
+      handleKeyStrokes();
+     }}
+     vimode={vimode}
+     language={language}
+    />
+    <GameAreaInfo.Container>
+     <GameAreaInfo.Box>
+      <GameAreaInfo.Text>
+       change the above to text to snippet given below
+      </GameAreaInfo.Text>
+      <DisplayEditor
+       className="code-display"
+       value={answer}
+       language={language}
+      />
+     </GameAreaInfo.Box>
+     <GameAreaInfo.ScoreContainer>
+      <GameAreaInfo.HugeScoreText>
+       Keystrokes required to change : {reqkeystrokes}
+      </GameAreaInfo.HugeScoreText>
+      <GameAreaInfo.HugeScoreText>
+       Global Score : {globalScore}
+      </GameAreaInfo.HugeScoreText>
+     </GameAreaInfo.ScoreContainer>
+     <div className="flex flex-row justify-around">
+      <div className="text-lg px-3 text-purple-100">Share Score :</div>
+      <RedditShareButton
+       url="https://vimgore.netlify.app"
+       title={ScoreShareString}
+      >
+       <RedditIcon className="rounded-full mx-1 w-10 h-10" />
+      </RedditShareButton>
+      <TwitterShareButton
+       url="https://vimgore.netlify.app"
+       title={ScoreShareString}
+      >
+       <TwitterIcon className="rounded-full mx-1 w-10 h-10" />
+      </TwitterShareButton>
+      <TelegramShareButton
+       url="https://vimgore.netlify.app"
+       title={ScoreShareString}
+      >
+       <TelegramIcon className="rounded-full mx-1 w-10 h-10" />
+      </TelegramShareButton>
+     </div>
+    </GameAreaInfo.Container>
+   </GameAreaInfo.Grid>
+  </>
+ );
 };
 
 export default GameArea;
