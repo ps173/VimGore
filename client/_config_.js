4ee57f3af521b80131739909f981b56c36c0a471
ps173 <prathamsharma173@gmail.com>
2021-10-03 22:06:45 +0530

    Commited Changes

--- client/_config_.js
+++ client/_config_.js
@@ -1,6 +1,6 @@
 const _config_ = {
- "API_PATH":"http://localhost:5000/api/v1/random" ||
-  process.env.NEXT_PUBLIC_API_PATH,
+  "API_PATH": process.env.NEXT_PUBLIC_API_PATH ||
+    "http://localhost:5000/api/v1/random",
 };
 
 export default _config_;
